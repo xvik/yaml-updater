cb8a1068a007dbabe3fcc7804defa5a4fc6e1a62
Vyacheslav Rusakov <vyarus@gmail.com>
2021-05-07 17:52:00 +0700

    base model javadoc

--- src/main/java/ru/vyarus/yaml/config/updater/parse/model/YamlLine.java
+++ src/main/java/ru/vyarus/yaml/config/updater/parse/model/YamlLine.java
@@ -1,9 +1,13 @@
-package ru.vyarus.yaml.config.updater.parse.api;
+package ru.vyarus.yaml.config.updater.parse.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Base class for comments and structure parsers. In both cases model represent one or more yaml file lines.
+ * For lists, object in list item is split to first property - list value and other props - its children
+ * (this way lines hierarchy is preserved, ignoring objects consistency as not important).
+ *
  * @author Vyacheslav Rusakov
  * @since 07.05.2021
  */
@@ -13,9 +17,10 @@ public class YamlLine<T extends YamlLine> {
     private final int padding;
     private String key;
     private boolean listValue;
-    private List<T> children = new ArrayList<>();
+    private final List<T> children = new ArrayList<>();
 
-    public YamlLine(T root, int padding) {
+    @SuppressWarnings("unchecked")
+    public YamlLine(final T root, final int padding) {
         this.root = root;
         this.padding = padding;
         if (root != null) {
@@ -35,7 +40,7 @@ public class YamlLine<T extends YamlLine> {
         return key;
     }
 
-    public void setKey(String key) {
+    public void setKey(final String key) {
         this.key = key;
     }
 
@@ -43,7 +48,7 @@ public class YamlLine<T extends YamlLine> {
         return listValue;
     }
 
-    public void setListValue(boolean listValue) {
+    public void setListValue(final boolean listValue) {
         this.listValue = listValue;
     }
 
