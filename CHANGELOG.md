e7d415806df89efe55defa04fa0c3a6e3a59fe96
Vyacheslav Rusakov <vyarus@gmail.com>
2021-08-30 17:44:13 +0700

    fix dropwizard 1.x compatibility, make dropwizard dependency propvided

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+* [dropwizard]
+  - Fix dropwizard 1.x compatibility
+  - Make dropwizard dependency "provided" (to avoid version clashes)
+
 ### 1.0.0 (2021-08-29)
 * Initial release
--- dropwizard-config-updater/src/main/java/ru/vyarus/yaml/updater/dropwizard/ConfigUpdaterBundle.java
+++ dropwizard-config-updater/src/main/java/ru/vyarus/yaml/updater/dropwizard/ConfigUpdaterBundle.java
@@ -3,6 +3,7 @@ package ru.vyarus.yaml.updater.dropwizard;
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 import ru.vyarus.yaml.updater.dropwizard.cmd.UpdateConfigCommand;
 
 /**
@@ -14,5 +15,11 @@ public class ConfigUpdaterBundle implements ConfiguredBundle<Configuration> {
     @Override
     public void initialize(final Bootstrap<?> bootstrap) {
         bootstrap.addCommand(new UpdateConfigCommand());
+    }
+
+    // added for dropwizard 1 compatibility
+    @Override
+    public void run(final Configuration configuration, final Environment environment) throws Exception {
+        // empty
     }
 }
